d999219443a4b951564ce0a9e8d91c406deab2cd
Nnnoooaaahhhh <55807513+Nnnoooaaahhhh@users.noreply.github.com>
2022-03-03 13:08:38 +0100

    Add files via upload

--- DoubleLinkedList/DoubleLinkedList.py
+++ DoubleLinkedList/DoubleLinkedList.py
@@ -1,9 +1,10 @@
 import Node as Node
 
+
 class DoubleLinkedList:
     def __init__(self):
         self.head = None
-
+        self.tail = None
 
     def printList(self):
         Node = self.head
@@ -12,22 +13,20 @@ class DoubleLinkedList:
             Node = Node.next
         print("\n")
 
-
     def append(self, newNodeData):
-            newNode = Node.Node(newNodeData)
-            newNode.next = None
-            if self.head is None:
-                newNode.head = None
-                self.head = newNode
-                return
-            lastNode = self.head
-            while lastNode.next:
-                lastNode = lastNode.next
-            lastNode.next = newNode
-            newNode.prev = lastNode
+        newNode = Node.Node(newNodeData)
+        newNode.next = None
+        if self.head is None:
+            newNode.head = None
+            self.head = newNode
+            return
+        lastNode = self.head
+        while lastNode.next:
+            lastNode = lastNode.next
+        lastNode.next = newNode
+        newNode.prev = lastNode
 
-
-    def insertAfter(self, node, newNodeData):
+    def insertAfterIndex(self, node, newNodeData):
         if node is None:
             print("Error, prevNode is None")
             return
@@ -47,28 +46,66 @@ class DoubleLinkedList:
         if newNode.next:
             newNode.next.prev = newNode
 
+    def insertAfterNode(self, prevNode, newNodeData):
+        if prevNode is None:
+            newNode = Node.Node(newNodeData)
+            self.head.prev = newNode
+            newNode.next = self.head
+            self.head = newNode
+            return
+        newNode = Node.Node(newNodeData)
+        newNode.next = prevNode.next
+        prevNode.next = newNode
+        newNode.prev = prevNode
+        if newNode.next:
+            newNode.next.prev = newNode
 
-    def insertBefore(self, node, newNodeData):
+    def insertBeforeIndex(self, node, newNodeData):
         if node is None:
             print("Error, prevNode is None")
             return
-        if node < 2:
-            print("Error, index must be > 1")
+        if node < 1:
+            print("Error, index must be > 0")
             return
         if node > self.length():
             print("Error, index too big")
             return
         befNode = self.head
+        newNode = Node.Node(newNodeData)
         for i in range(node - 1):
             befNode = befNode.next
-        newNode = Node.Node(newNodeData)
+        if befNode is self.head:
+            a = self.head
+            self.head = newNode
+            newNode.next = a
+            a.prev = newNode
+            newNode.prev = None
+            return
         newNode.prev = befNode.prev
         befNode.prev = newNode
         newNode.next = befNode
         if newNode.prev:
             newNode.prev.next = newNode
 
-    def deleteAfter(self, node):
+    def insertBeforeNode(self, befNode, newNodeData):
+        newNode = Node.Node(newNodeData)
+        if befNode is None:
+            print("Node falsch!")
+            return
+        if befNode is self.head:
+            a = self.head
+            self.head = newNode
+            newNode.next = a
+            a.prev = newNode
+            newNode.prev = None
+            return
+        newNode.prev = befNode.prev
+        befNode.prev = newNode
+        newNode.next = befNode
+        if newNode.prev:
+            newNode.prev.next = newNode
+
+    def deleteAfterIndex(self, node):
         if node is None:
             print("Error, prevNode is None")
             return
@@ -79,15 +116,27 @@ class DoubleLinkedList:
             print("Error, index must be > 0")
             return
         prevNode = self.head
-        for i in range(node-1):
+        for i in range(node - 1):
             prevNode = prevNode.next
         deleteNode = prevNode.next
         afterNode = deleteNode.next
         prevNode.next = deleteNode.next
         afterNode.prev = deleteNode.prev
 
+    def deleteAfterNode(self, prevNode):
+        if prevNode.next is None:
+            print("Node falsch! (After)")
+            return
+        prevNode = prevNode
+        deleteNode = prevNode.next
+        afterNode = None
+        if deleteNode.next is not None:
+            afterNode = deleteNode.next
+        prevNode.next = deleteNode.next
+        if afterNode is not None:
+            afterNode.prev = deleteNode.prev
 
-    def deleteBefore(self, node):
+    def deleteBeforeIndex(self, node):
         if node is None:
             print("Error, prevNode is None")
             return
@@ -105,47 +154,81 @@ class DoubleLinkedList:
         befNode.prev = deleteNode.prev
         afterNode.next = deleteNode.next
 
+    def deleteBeforeNode(self, afterNode):
+        if afterNode.prev is None:
+            print("Node falsch! (Before)")
+            return
+        afterNode = afterNode
+        deleteNode = afterNode.prev
+        beforeNode = deleteNode.prev
+        beforeNode.next = afterNode
+        afterNode.prev = afterNode
 
     def find(self, NodeToFind):
         Node = self.head
         index = 0
         while Node:
-            if(Node.data == NodeToFind.data):
+            if Node.data is NodeToFind.data:
                 print("found at index " + str(index))
                 return
             index += 1
-            last = Node
             Node = Node.next
         print("not found")
-
 
     def length(self):
         tempNode = self.head
         len = 0
-        while tempNode != None:
+        while tempNode is not None:
             len += 1
             tempNode = tempNode.next
         return len
-#
 
     def max(self):
         tempNode = self.head
         max = tempNode.data
-        while tempNode != None:
+        while tempNode is not None:
             if tempNode.data > max:
                 max = tempNode.data
             tempNode = tempNode.next
         return max
 
-
     def min(self):
         tempNode = self.head
         min = tempNode.data
-        while tempNode != None:
+        while tempNode is not None:
             if tempNode.data < min:
                 min = tempNode.data
             tempNode = tempNode.next
         return min
 
 
+    def insSortAsc(self):
+        key = self.head
+        while key is not None:
+            start = self.head
+            while key is not start:
+                if key.data < start.data:
+                    self.insertAfterNode(start.prev, key.data)
+                    self.deleteAfterNode(key.prev)
+                    break
+                start = start.next
+            key = key.next
 
+
+    def insSortDesc(self):
+        key = self.head
+        while key is not None:
+            start = self.head
+            while key is not start:
+                if key.data > start.data:
+                    self.insertAfterNode(start.prev, key.data)
+                    self.deleteAfterNode(key.prev)
+                    break
+                start = start.next
+            key = key.next
+
+    def getNode(self, index):
+        tempNode = self.head
+        for i in range(index):
+            tempNode = tempNode.next
+        return tempNode
--- DoubleLinkedList/main.py
+++ DoubleLinkedList/main.py
@@ -1,17 +1,32 @@
 import DoubleLinkedList as dll
 import random
-import Node
 
 if __name__ == "__main__":
     print("main")
     doubleLinkedList = dll.DoubleLinkedList()
-    for i in range(100):
+    for i in range(50):
         doubleLinkedList.append(random.randint(0, 100))
-    print(doubleLinkedList.length())
-    #doubleLinkedList.insertAfter(6, 323)
-    #doubleLinkedList.insertBefore(6, 334)
-    #doubleLinkedList.deleteAfter(5)
-    #doubleLinkedList.deleteBefore(7)
+    """doubleLinkedList.append(1)
+    doubleLinkedList.append(2)
+    doubleLinkedList.append(6)
+    doubleLinkedList.append(5)
+    doubleLinkedList.append(2)"""
+    """doubleLinkedList.append(3)
+    doubleLinkedList.append(2)
+    doubleLinkedList.append(1)"""
+    doubleLinkedList.printList()
+    #doubleLinkedList.insertBeforeNode(doubleLinkedList.head, 9)
+    #doubleLinkedList.insertAfterNode(doubleLinkedList.head.next.next.next, 1)
+    #doubleLinkedList.insertAfterIndex(1, 323)
+    #doubleLinkedList.insertBeforeIndex(45, 384)
+    #doubleLinkedList.deleteAfterNode(doubleLinkedList.head.next)
+    #doubleLinkedList.deleteBeforeNode(doubleLinkedList.head.next)
     #doubleLinkedList.find(Node.Node(45))
-    print(doubleLinkedList.length())
+    doubleLinkedList.insSortAsc()
+    doubleLinkedList.printList()
+    doubleLinkedList.insSortDesc()
+    #doubleLinkedList.printList()
+    #doubleLinkedList.insSortDesc()
+    #print(doubleLinkedList.getNode(0).data)
+    #print(doubleLinkedList.length())
     doubleLinkedList.printList()
