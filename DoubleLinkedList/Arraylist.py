348a95bef9d42b82cad0ce9af1e7fc007d80d659
Nnnoooaaahhhh <55807513+Nnnoooaaahhhh@users.noreply.github.com>
2022-03-15 10:30:41 +0100

    Add files via upload

---
+++ DoubleLinkedList/Arraylist.py
@@ -0,0 +1,89 @@
+class Arraylist:
+    def __init__(self):
+        self.list = []
+
+    def append(self, value):
+        self.list.append(value)
+
+    def getLength(self):
+        return len(self.list)
+
+    def printAll(self):
+        for i in self.list:
+            print(str(i), end=" ")
+
+    def getMax(self):
+        return max(self.list)
+
+    def getMin(self):
+        return min(self.list)
+
+    def deleteBefore(self, index):
+        if index is None:
+            return
+        elif index <= 0:
+            print("Can't delete before 0")
+            return
+        elif index > self.getLength():
+            print("Index too big")
+            return
+        else:
+            self.list.pop(index-1)
+
+    def deleteAfter(self, index):
+        if index is None:
+            return
+        elif index < 0:
+            print("Can't delete after negativ index")
+            return
+        elif index > self.getLength()-2:
+            print("Index too big")
+            return
+        else:
+            self.list.pop(index+1)
+
+    def insertBefore(self, index, value):
+        if index is None:
+            return
+        elif index < 0:
+            print("Can't delete before 0")
+            return
+        elif index > self.getLength() - 1:
+            print("Index too big")
+            return
+        else:
+            self.list.insert(index, value)
+
+
+    def insertAfter(self, index, value):
+        if index is None:
+            return
+        elif index < 0:
+            print("Can't delete after negativ index")
+            return
+        elif index > self.getLength()-1:
+            print("Index too big")
+            return
+        else:
+            self.list.insert(index +  1, value)
+
+
+    def sortAsc(self):
+        for i in range(1, self.getLength()):
+            key = self.list[i]
+            j = i - 1
+            while j >= 0 and key < self.list[j]:
+                self.list[j + 1] = self.list[j]
+                j -= 1
+            self.list[j + 1] = key
+
+
+
+    def sortDesc(self):
+        for i in range(1, self.getLength()):
+            key = self.list[i]
+            j = i-1
+            while j>= 0 and key > self.list[j]:
+                self.list[j+1] = self.list[j]
+                j -= 1
+            self.list[j+1] = key
--- DoubleLinkedList/main.py
+++ DoubleLinkedList/main.py
@@ -1,20 +1,46 @@
 import DoubleLinkedList as dll
 import random
+import Arraylist as al
+import time
 
 if __name__ == "__main__":
     print("main")
     doubleLinkedList = dll.DoubleLinkedList()
-    for i in range(50):
-        doubleLinkedList.append(random.randint(0, 100))
-    """doubleLinkedList.append(1)
-    doubleLinkedList.append(2)
-    doubleLinkedList.append(6)
-    doubleLinkedList.append(5)
-    doubleLinkedList.append(2)"""
-    """doubleLinkedList.append(3)
-    doubleLinkedList.append(2)
-    doubleLinkedList.append(1)"""
+    arrayList = al.Arraylist()
+    for i in range(10000):
+        x = random.randint(0, 10000)
+        doubleLinkedList.append(x)
+        arrayList.append(x)
+    arrayList.printAll()
+    print("\n")
     doubleLinkedList.printList()
+    start = time.time()
+    doubleLinkedList.insSortAsc()
+    end = time.time()
+    print(end-start)
+    start = time.time()
+    arrayList.sortAsc()
+    end = time.time()
+    print(end - start)
+    arrayList.printAll()
+    print("\n")
+    doubleLinkedList.printList()
+
+    #ArrayList Methoden
+    #arrayList.append(34)
+    #arrayList.append(10)
+    #arrayList.append(283)
+    #print(arrayList.getMax())
+    #print(arrayList.getMin())
+    #arrayList.deleteBefore(4)
+    #arrayList.deleteAfter(1)
+    #print(arrayList.getLength())
+    #arrayList.insertBefore(2, 199)
+    #arrayList.insertAfter(0, 129)
+    #arrayList.sortDesc()
+    #arrayList.sortAsc()
+
+    #Double Linked List Methoden
     #doubleLinkedList.insertBeforeNode(doubleLinkedList.head, 9)
     #doubleLinkedList.insertAfterNode(doubleLinkedList.head.next.next.next, 1)
     #doubleLinkedList.insertAfterIndex(1, 323)
@@ -22,11 +48,8 @@ if __name__ == "__main__":
     #doubleLinkedList.deleteAfterNode(doubleLinkedList.head.next)
     #doubleLinkedList.deleteBeforeNode(doubleLinkedList.head.next)
     #doubleLinkedList.find(Node.Node(45))
-    doubleLinkedList.insSortAsc()
-    doubleLinkedList.printList()
-    doubleLinkedList.insSortDesc()
-    #doubleLinkedList.printList()
+    #doubleLinkedList.insSortAsc()
     #doubleLinkedList.insSortDesc()
-    #print(doubleLinkedList.getNode(0).data)
     #print(doubleLinkedList.length())
-    doubleLinkedList.printList()
+    #print(doubleLinkedList.Max())
+    #print(doubleLinkedList.min())
